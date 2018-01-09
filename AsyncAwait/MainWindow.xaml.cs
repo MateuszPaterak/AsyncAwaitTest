e830662133493b352d5a46c27a651241fcc930a3
MateuszPaterak <mzt@onet.eu>
2018-01-09 01:00:36 +0100

    add project

---
+++ AsyncAwait/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //async-await
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = await TestIntAsync();
+            MessageBox.Show(x.ToString());
+        }
+
+        private async Task<int> TestIntAsync()
+        {
+            return await Task.Run(
+                () =>{
+                    Thread.Sleep(5000);
+                    return 5;
+                });
+        }
+        //classical, with blocking gui
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.Sleep(5000);
+            int x = 5;
+            MessageBox.Show(x.ToString());
+        }
+
+        //blocking gui too
+        private async void button2_Click(object sender, RoutedEventArgs e)
+        {
+            int x = await TestAsyncInt2();
+            MessageBox.Show(x.ToString());
+        }
+
+        private async Task<int> TestAsyncInt2() //block gui
+        {
+            Thread.Sleep(5000);
+            return 5;
+        }
+
+        //2s+2s+2s
+        private async void button3_Click(object sender, RoutedEventArgs e)
+        {
+            int thread = Thread.CurrentThread.ManagedThreadId;
+            int? task = Task.CurrentId;
+            textBox.Text = $"T0;Th{thread};Ta{task} \n";
+            await Task.Run(() =>
+            { 
+                Thread.Sleep(2000);
+                thread = Thread.CurrentThread.ManagedThreadId;
+                task = Task.CurrentId;
+                textBox.Dispatcher.Invoke(() => { textBox.Text += $"T1;Th{thread};Ta{task} \n"; });
+            });
+            
+            await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                thread = Thread.CurrentThread.ManagedThreadId;
+                task = Task.CurrentId;
+                textBox.Dispatcher.Invoke(() => { textBox.Text += $"T2;Th{thread};Ta{task} \n"; });
+            });
+            
+            await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                thread = Thread.CurrentThread.ManagedThreadId;
+                task = Task.CurrentId;
+                textBox.Dispatcher.Invoke(() => { textBox.Text += $"T3;Th{thread};Ta{task} \n"; });
+            });
+            
+        }
+    }
+}
