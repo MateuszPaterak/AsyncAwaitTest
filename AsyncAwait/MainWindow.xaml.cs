99bd8907c1ed94ab78258dc9f2446562a2b16d0f
MateuszPaterak <mzt@onet.eu>
2018-01-18 01:21:42 +0100

    action correction

--- AsyncAwait/MainWindow.xaml.cs
+++ AsyncAwait/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AsyncAwait
 {
@@ -33,20 +22,10 @@ namespace AsyncAwait
             DisplayClear();
             DisplayThreadInfo("main");
             textBox.Text += "before await\n";
-            int x = await TestIntAsync();
+            int x = await Sleep3sAsync();
             textBox.Text += $"End {x}s";
         }
 
-        private async Task<int> TestIntAsync()
-        {
-            return await Task.Run( 
-                () =>{
-                    DisplayThreadInfo("T1");
-                    Thread.Sleep(5000);
-                    return 5;
-                });
-        }
-        
         //classical, with blocking gui
         private void button1_Click(object sender, RoutedEventArgs e)
         {
@@ -63,34 +42,28 @@ namespace AsyncAwait
             DisplayClear();
             DisplayThreadInfo("main");
             textBox.Text += "Start blocking for 5s\n";
-            int x = await TestAsyncInt2();
+            int x = await SleepWithoutCreateTask();
             textBox.Text += "End\n";
         }
-
-        private async Task<int> TestAsyncInt2() //block gui
-        {//without create task
-            Thread.Sleep(5000);
-            return 5;
-        }
-
+        
         //2s+2s+2s
         private async void button3_Click(object sender, RoutedEventArgs e)
         {
             DisplayClear();
             DisplayThreadInfo("main");
-            await SleepAsync("T1");
-            await SleepAsync("T2");
-            await SleepAsync("T3");
+            await Sleep2sAsync("T1");
+            await Sleep2sAsync("T2");
+            await Sleep2sAsync("T3");
 
             textBox.Text += "end\n";
         }
-        private async void button5_Click(object sender, RoutedEventArgs e)
+        private async void button4_Click(object sender, RoutedEventArgs e)
         {
             DisplayClear();
             DisplayThreadInfo("main");
-            var s1 = SleepAsync("T1");
-            var s2 = SleepAsync("T2");
-            var s3 = SleepAsync("T3");
+            var s1 = Sleep2sAsync("T1");
+            var s2 = Sleep2sAsync("T2");
+            var s3 = Sleep2sAsync("T3");
 
             string d1 = await s1;
             string d2 = await s2;
@@ -98,33 +71,47 @@ namespace AsyncAwait
             textBox.Text += $"end {d1}, {d2}, {d3}\n";
         }
 
-        private async Task<string> SleepAsync(string name)
+        private async void button5_Click(object sender, RoutedEventArgs e)
         {
-            return await Task.Run(() =>
-            {
-                Thread.Sleep(2000);
-                DisplayThreadInfo(name);
-                return name;
-            });
+            await ParallelSleepAsync();
         }
-        
-        private async void button4_Click(object sender, RoutedEventArgs e)
+
+        private async Task<int> Sleep3sAsync()
         {
-            await TestAsync();
+            return await Task.Run(
+                () => {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        DisplayThreadInfo($"T1 {i}s");
+                        Thread.Sleep(1000 * (i + 1));
+                    }
+                    return 5;
+                });
         }
-        private async Task TestAsync()
+
+        private async Task ParallelSleepAsync()
         {
-            textBox.Text = "before TestIntAsync() \n";
-            Task<int> result =  TestIntAsync();
+            DisplayClear();
+            DisplayThreadInfo("main");
+            textBox.Text += "before TestIntAsync() \n";
+            Task<int> result = Sleep3sAsync();
             textBox.Text += "after TestIntAsync() \n";
-            textBox.Text += "sleep 3s\n";
-            Thread.Sleep(3000);
+            for (int i = 0; i < 3; i++)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                textBox.Text += "sleep 1s\n";
+            }
 
             textBox.Text += "before 'await result' \n";
             var a = await result;
             textBox.Text += "after 'await result' \n";
             textBox.Text += a + "\n";
             textBox.Text += "after print result \n";
+        }
+        private async Task<int> SleepWithoutCreateTask() //block gui
+        {
+            Thread.Sleep(5000);
+            return 5;
         }
 
         private void DisplayThreadInfo(string name)
@@ -133,6 +120,15 @@ namespace AsyncAwait
             int? task = Task.CurrentId;
             textBox.Dispatcher.Invoke(() => { textBox.Text += $"{name}; Thread:{thread}; Task:{task}; \n"; });
             //textBox.Dispatcher.InvokeAsync(() => { textBox.Text += $"{name}; Thread:{thread}; Task:{task}; \n"; });
+        }
+        private async Task<string> Sleep2sAsync(string name)
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                DisplayThreadInfo(name);
+                return name;
+            });
         }
 
         private void DisplayClear()
